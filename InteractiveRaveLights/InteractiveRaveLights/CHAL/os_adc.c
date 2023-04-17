0aef6f60395e4d9ab84feafbb07d4ed5f922004f
William Redenbaugh <wmredenbaugh@gmail.com>
2023-04-17 04:09:23 -0700

    implemented the UART and ADC HAL layer modules

--- InteractiveRaveLights/InteractiveRaveLights/CHAL/os_adc.c
+++ InteractiveRaveLights/InteractiveRaveLights/CHAL/os_adc.c
@@ -1,2 +1,134 @@
 #include "os_adc.h"
+#include <nuttx/config.h>
+#include <stdio.h>
+#include "thread_init/threads_init.h"
+#include "pthread.h"
+#include "CSAL/os_error.h"
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include "pthread.h"
+#include <arch/chip/scu.h>
+#include <arch/chip/adc.h>
+#include "nuttx/wqueue.h"
 
+os_adc_t adc_one = {
+    .bus = 1, 
+    .num_bits = 16,
+    .fifosize = 128
+};
+os_adc_t adc_two = {
+    .bus = 2, 
+    .num_bits = 16,
+    .fifosize = 128
+};
+os_adc_t adc_three = {
+    .bus = 3, 
+    .num_bits = 16,
+    .fifosize = 128
+};
+os_adc_t adc_four = {
+    .bus = 4, 
+    .num_bits = 16,
+    .fifosize = 128
+};
+os_adc_t adc_five = {
+    .bus = 5, 
+    .num_bits = 16,
+    .fifosize = 128
+};
+os_adc_t adc_size = {
+    .bus = 6, 
+    .num_bits = 16,
+    .fifosize = 128
+};
+
+int os_adc_begin(os_adc_t *adc){
+        
+    if(adc == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    int errval, errno; 
+
+    if(adc)
+    switch(adc->bus){
+        case 1:
+            adc->fd = open("/dev/hpadc0", O_RDONLY);
+        break;
+        case 2:
+            adc->fd = open("/dev/hpadc1", O_RDONLY);
+        break;
+        case 3:
+            adc->fd = open("/dev/lpadc0", O_RDONLY);
+        break;
+        case 4:
+            adc->fd = open("/dev/lpadc1", O_RDONLY);
+        break;
+        case 5:
+            adc->fd = open("/dev/lpadc2", O_RDONLY);
+        break;
+        case 6:
+            adc->fd = open("/dev/lpadc3", O_RDONLY);
+        break;
+    }
+
+    if (adc->fd < 0){
+        errval = 4;
+        return OS_RET_NO_MORE_RESOURCES;
+    }
+
+    int ret = ioctl(adc->fd, SCUIOC_SETFIFOMODE, 1);
+    if (ret < 0){
+        close(adc->fd);
+        return OS_RET_LOW_MEM_ERROR;
+    }
+
+    ret = ioctl(adc->fd, ANIOC_CXD56_FIFOSIZE, adc->fifosize * 2);
+    if (ret < 0){
+        close(adc->fd);
+        return OS_RET_LOW_MEM_ERROR;
+    }
+
+    ret = ioctl(adc->fd, ANIOC_CXD56_START, 0);
+    if (ret < 0){
+        close(adc->fd);
+        return OS_RET_UNDEFINED_ERROR;
+    }
+
+    return OS_RET_OK;
+}
+
+int os_adc_end(os_adc_t *adc){
+        
+    if(adc == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    int ret = ioctl(adc->fd, ANIOC_CXD56_STOP, 0);
+    if (ret < 0){
+        return OS_RET_INT_ERR;
+    }
+
+    close(adc->fd);
+    return OS_RET_OK;
+}
+
+int os_adc_read(os_adc_t *adc, void *value){
+    read(adc->fd, value, 2);
+    return OS_RET_OK;
+}
+
+int os_adc_read_values(os_adc_t *adc, uint32_t size, void *values){
+
+    if(adc == NULL){
+        return OS_RET_NULL_PTR;
+    }
+    read(adc->fd, values, size *2);
+
+    return OS_RET_OK;
+}
--- InteractiveRaveLights/InteractiveRaveLights/CHAL/os_uart.c
+++ InteractiveRaveLights/InteractiveRaveLights/CHAL/os_uart.c
@@ -1,26 +1,111 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sched.h>
+#include <errno.h>
+#include "nuttx/fs/ioctl.h"
+#include "nuttx/serial/tioctl.h"
+#include "termios.h"
+#include "stdint.h"
+
 #include "os_uart.h"
 #include "CSAL/os_error.h"
 
 int os_uart_begin(os_uart_t *uart){
-    
+    if(uart == NULL)
+        return OS_RET_NULL_PTR;   
+
+    switch(uart->bus){
+        case(0):
+            uart->fd = open("/dev/ttyS0", O_RDWR);
+        case(2):
+            uart->fd = open("dev/ttyS2", O_RDWR);
+        default:
+            uart->fd = -1;
+            return OS_RET_INVALID_PARAM;
+    } 
+
+    struct termios tio;
+    /* tty: setup parameters */
+    tio.c_cflag |= CREAD;   /* Enable receive */
+    tio.c_cflag |= CLOCAL;  /* Local line, no modem control */
+    tio.c_cflag &= ~CSIZE;  /* Clean the bits */
+    tio.c_cflag |= CS8;     /* Data bit 8bit */
+    tio.c_cflag &= ~CSTOPB; /* Stop bit 1bit */
+    tio.c_cflag &= ~PARENB; /* Paritiy none */
+
+    tio.c_cc[VMIN] = 1;
+    tio.c_cc[VTIME] = 5;
+    cfsetspeed(&tio, uart->baud);
+
+    if (tcsetattr(uart->fd, TCSANOW, &tio)){
+        printf("Error setting uart attributes\n");
+    }
+    else{
+        printf("Success in setting UART attributes\n");
+    }
+    const int bits = TIOCM_RTS;
+    int ret = ioctl(uart->fd, TIOCMBIS, (unsigned long)&bits);
+
+    if(ret < 0){
+        return OS_RET_INT_ERR;
+    }
+
+    return OS_RET_OK;
 }
 
 int os_uart_end(os_uart_t *uart){
-    
+    if(uart == NULL)
+        return OS_RET_NULL_PTR;
 }
 
 int os_uart_setbus(os_uart_t *uart, uint32_t freq_baud){
-    
+    if(uart == NULL)
+        return OS_RET_NULL_PTR;
+
+    uart->baud = freq_baud;
+
+    struct termios tio;
+    /* tty: setup parameters */
+    tio.c_cflag |= CREAD;   /* Enable receive */
+    tio.c_cflag |= CLOCAL;  /* Local line, no modem control */
+    tio.c_cflag &= ~CSIZE;  /* Clean the bits */
+    tio.c_cflag |= CS8;     /* Data bit 8bit */
+    tio.c_cflag &= ~CSTOPB; /* Stop bit 1bit */
+    tio.c_cflag &= ~PARENB; /* Paritiy none */
+
+    tio.c_cc[VMIN] = 1;
+    tio.c_cc[VTIME] = 5;
+    cfsetspeed(&tio, uart->baud);
+
+    if (tcsetattr(uart->fd, TCSANOW, &tio)){
+        printf("Error setting uart attributes\n");
+    }
+    else{
+        printf("Success in setting UART attributes\n");
+    }
+    const int bits = TIOCM_RTS;
+    int ret = ioctl(uart->fd, TIOCMBIS, (unsigned long)&bits);
 }
 
 int os_uart_transfer(os_uart_t *uart, uint8_t *rx, uint8_t*tx, size_t size){
-    
+    if(uart == NULL)
+        return OS_RET_NULL_PTR;
+
+    return OS_RET_OK;
 }
 
 int os_uart_send(os_uart_t *uart, uint8_t *buf, size_t size){
-    
+    if(uart == NULL)
+        return OS_RET_NULL_PTR;
+
+    write(uart->fd, buf, size);
 }
 
 int os_uart_recieve(os_uart_t *uart, uint8_t *buf, size_t size){
-    
+    if(uart == NULL)
+        return OS_RET_NULL_PTR;
+
+    read(uart->fd, buf, size);
 }
